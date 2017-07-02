1d9908c66995955efdc3433ee4e566f147bd034b
John Nesky <johnnesky@gmail.com>
2017-07-01 18:31:24 -0700

    Minifying beepbox_synth.js

--- compile_beepbox_synth.sh
+++ compile_beepbox_synth.sh
@@ -1,3 +1,22 @@
 #!/bin/bash
 
-tsc --target ES5 ts/synth.ts --out beepbox-synth/beepbox_synth.js;
+# Compile ts/synth.ts into beepbox_synth.js
+#	--strictNullChecks \
+tsc \
+	--target ES5 \
+	--noImplicitAny \
+	--noImplicitReturns \
+	--noFallthroughCasesInSwitch \
+	--removeComments \
+	ts/synth.ts \
+	--out beepbox-synth/beepbox_synth.js
+
+# Minify beepbox_synth.js into beepbox_synth.min.js
+uglifyjs \
+	--compress \
+	--mangle \
+	--mangle-props \
+	--mangle-regex="/^_.+/" \
+	--screw-ie8 \
+	beepbox-synth/beepbox_synth.js \
+	-o beepbox-synth/beepbox_synth.min.js
