bc142fbce7f73de362654ef6cad9c37b2dc89096
John Nesky <johnnesky@gmail.com>
2018-09-12 22:57:39 -0700

    Pulled bar count into its own prompt, can add bars at beginning or end of song.

--- ts/SongDurationPrompt.ts
+++ ts/SongDurationPrompt.ts
@@ -27,42 +27,29 @@ SOFTWARE.
 /// <reference path="changes.ts" />
 
 namespace beepbox {
-	const {button, div, span, input, br, text} = html;
+	const {button, div, span, input, br, text, select, option} = html;
 	
 	export class SongDurationPrompt implements Prompt {
 		private readonly _barsStepper: HTMLInputElement = input({style: "width: 3em; margin-left: 1em;", type: "number", step: "1"});
-		private readonly _patternsStepper: HTMLInputElement = input({style: "width: 3em; margin-left: 1em;", type: "number", step: "1"});
-		private readonly _instrumentsStepper: HTMLInputElement = input({style: "width: 3em; margin-left: 1em;", type: "number", step: "1"});
-		private readonly _pitchChannelStepper: HTMLInputElement = input({style: "width: 3em; margin-left: 1em;", type: "number", step: "1"});
-		private readonly _drumChannelStepper: HTMLInputElement = input({style: "width: 3em; margin-left: 1em;", type: "number", step: "1"});
+		private readonly _positionSelect: HTMLSelectElement = select({style: "width: 100%;"}, [
+			option("end",       "Apply change at end of song."),
+			option("beginning", "Apply change at beginning of song."),
+		]);
 		private readonly _okayButton: HTMLButtonElement = button({style: "width:45%;"}, [text("Okay")]);
 		private readonly _cancelButton: HTMLButtonElement = button({style: "width:45%;"}, [text("Cancel")]);
 		
 		public readonly container: HTMLDivElement = div({className: "prompt", style: "width: 250px;"}, [
-			div({style: "font-size: 2em"}, [text("Custom Song Size")]),
+			div({style: "font-size: 2em"}, [text("Song Length")]),
 			div({style: "display: flex; flex-direction: row; align-items: center; height: 2em; justify-content: flex-end;"}, [
 				div({style: "display: inline-block; text-align: right;"}, [
 					text("Bars per song:"),
 					br(),
-					span({style: "font-size: smaller; color: #888888;"}, [text("(Multiples of 2 or 4 are recommended)")]),
+					span({style: "font-size: smaller; color: #888888;"}, [text("(Multiples of 4 are recommended)")]),
 				]),
 				this._barsStepper,
 			]),
 			div({style: "display: flex; flex-direction: row; align-items: center; height: 2em; justify-content: flex-end;"}, [
-				text("Patterns per channel:"),
-				this._patternsStepper,
-			]),
-			div({style: "display: flex; flex-direction: row; align-items: center; height: 2em; justify-content: flex-end;"}, [
-				text("Instruments per channel:"),
-				this._instrumentsStepper,
-			]),
-			div({style: "display: flex; flex-direction: row; align-items: center; height: 2em; justify-content: flex-end;"}, [
-				text("Number of pitch channels:"),
-				this._pitchChannelStepper,
-			]),
-			div({style: "display: flex; flex-direction: row; align-items: center; height: 2em; justify-content: flex-end;"}, [
-				text("Number of drum channels:"),
-				this._drumChannelStepper,
+				div({className: "selectContainer", style: "width: 100%;"}, [this._positionSelect]),
 			]),
 			div({style: "display: flex; flex-direction: row; justify-content: space-between;"}, [
 				this._okayButton,
@@ -75,53 +62,21 @@ namespace beepbox {
 			this._barsStepper.min = Config.barCountMin + "";
 			this._barsStepper.max = Config.barCountMax + "";
 			
-			this._patternsStepper.value = this._doc.song.patternsPerChannel + "";
-			this._patternsStepper.min = Config.patternsPerChannelMin + "";
-			this._patternsStepper.max = Config.patternsPerChannelMax + "";
-			
-			this._instrumentsStepper.value = this._doc.song.instrumentsPerChannel + "";
-			this._instrumentsStepper.min = Config.instrumentsPerChannelMin + "";
-			this._instrumentsStepper.max = Config.instrumentsPerChannelMax + "";
-			
-			this._pitchChannelStepper.value = this._doc.song.pitchChannelCount + "";
-			this._pitchChannelStepper.min = Config.pitchChannelCountMin + "";
-			this._pitchChannelStepper.max = Config.pitchChannelCountMax + "";
-			
-			this._drumChannelStepper.value = this._doc.song.drumChannelCount + "";
-			this._drumChannelStepper.min = Config.drumChannelCountMin + "";
-			this._drumChannelStepper.max = Config.drumChannelCountMax + "";
-			
 			this._okayButton.addEventListener("click", this._saveChanges);
 			this._cancelButton.addEventListener("click", this._close);
 			this._barsStepper.addEventListener("keypress", SongDurationPrompt._validateKey);
-			this._patternsStepper.addEventListener("keypress", SongDurationPrompt._validateKey);
-			this._instrumentsStepper.addEventListener("keypress", SongDurationPrompt._validateKey);
-			this._pitchChannelStepper.addEventListener("keypress", SongDurationPrompt._validateKey);
-			this._drumChannelStepper.addEventListener("keypress", SongDurationPrompt._validateKey);
 			this._barsStepper.addEventListener("blur", SongDurationPrompt._validateNumber);
-			this._patternsStepper.addEventListener("blur", SongDurationPrompt._validateNumber);
-			this._instrumentsStepper.addEventListener("blur", SongDurationPrompt._validateNumber);
-			this._pitchChannelStepper.addEventListener("blur", SongDurationPrompt._validateNumber);
-			this._drumChannelStepper.addEventListener("blur", SongDurationPrompt._validateNumber);
 		}
 		
 		private _close = (): void => { 
 			this._doc.undo();
 		}
 		
-		public cleanUp = (): void => { 
+		public cleanUp = (): void => {
 			this._okayButton.removeEventListener("click", this._saveChanges);
 			this._cancelButton.removeEventListener("click", this._close);
 			this._barsStepper.removeEventListener("keypress", SongDurationPrompt._validateKey);
-			this._patternsStepper.removeEventListener("keypress", SongDurationPrompt._validateKey);
-			this._instrumentsStepper.removeEventListener("keypress", SongDurationPrompt._validateKey);
-			this._pitchChannelStepper.removeEventListener("keypress", SongDurationPrompt._validateKey);
-			this._drumChannelStepper.removeEventListener("keypress", SongDurationPrompt._validateKey);
 			this._barsStepper.removeEventListener("blur", SongDurationPrompt._validateNumber);
-			this._patternsStepper.removeEventListener("blur", SongDurationPrompt._validateNumber);
-			this._instrumentsStepper.removeEventListener("blur", SongDurationPrompt._validateNumber);
-			this._pitchChannelStepper.removeEventListener("blur", SongDurationPrompt._validateNumber);
-			this._drumChannelStepper.removeEventListener("blur", SongDurationPrompt._validateNumber);
 		}
 		
 		private static _validateKey(event: KeyboardEvent): boolean {
@@ -144,10 +99,7 @@ namespace beepbox {
 		
 		private _saveChanges = (): void => {
 			const group: ChangeGroup = new ChangeGroup();
-			group.append(new ChangeBarCount(this._doc, SongDurationPrompt._validate(this._barsStepper)));
-			group.append(new ChangePatternsPerChannel(this._doc, SongDurationPrompt._validate(this._patternsStepper)));
-			group.append(new ChangeInstrumentsPerChannel(this._doc, SongDurationPrompt._validate(this._instrumentsStepper)));
-			group.append(new ChangeChannelCount(this._doc, SongDurationPrompt._validate(this._pitchChannelStepper), SongDurationPrompt._validate(this._drumChannelStepper)));
+			group.append(new ChangeBarCount(this._doc, SongDurationPrompt._validate(this._barsStepper), this._positionSelect.value == "beginning"));
 			this._doc.prompt = null;
 			this._doc.record(group, true);
 		}
