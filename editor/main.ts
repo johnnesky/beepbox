ef0f306f6bffcb7969a19ee2253a0bd870337f87
John Nesky <johnnesky@gmail.com>
2024-07-07 23:34:32 -0700

    Simplified main.ts.

--- editor/main.ts
+++ editor/main.ts
@@ -1,48 +1,16 @@
 // Copyright (c) John Nesky and contributing authors, distributed under the MIT license, see accompanying the LICENSE.md file.
 
-import {Dictionary, DictionaryArray, EnvelopeType, InstrumentType, Transition, Chord, Envelope, Config} from "../synth/SynthConfig";
-import {EditorConfig} from "./EditorConfig";
-import {ColorConfig} from "./ColorConfig";
+export {Dictionary, DictionaryArray, EnvelopeType, InstrumentType, Transition, Chord, Envelope, Config} from "../synth/SynthConfig";
+export {EditorConfig} from "./EditorConfig";
+export {ColorConfig} from "./ColorConfig";
 import "./style"; // Import for the side effects, there's no exports.
-import {SongEditor} from "./SongEditor";
-import {NotePin, Note, Pattern, Instrument, Channel, Song, Synth} from "../synth/synth";
-import {SongDocument} from "./SongDocument";
-import {ExportPrompt} from "./ExportPrompt";
-import {ChangePreset} from "./changes";
-import {fastFourierTransform, forwardRealFourierTransform, inverseRealFourierTransform} from "../synth/FFT";
-import {FilterCoefficients, FrequencyResponse, DynamicBiquadFilter} from "../synth/filtering";
+export {SongEditor} from "./SongEditor";
+export {NotePin, Note, Pattern, Instrument, Channel, Song, Synth} from "../synth/synth";
+export {SongDocument} from "./SongDocument";
+export {ExportPrompt} from "./ExportPrompt";
+export {ChangePreset} from "./changes";
+export {fastFourierTransform, forwardRealFourierTransform, inverseRealFourierTransform} from "../synth/FFT";
+export {FilterCoefficients, FrequencyResponse, DynamicBiquadFilter} from "../synth/filtering";
 
 // To initialize:
 // new beepbox.SongEditor(document.getElementById("beepboxEditorContainer"));
-
-// When compiling editor/main.ts as a standalone module named "beepbox", expose
-// these classes as members to JavaScript:
-export {
-	Dictionary,
-	DictionaryArray,
-	EnvelopeType,
-	InstrumentType,
-	Transition,
-	Chord,
-	Envelope,
-	Config,
-	NotePin,
-	Note,
-	Pattern,
-	Instrument,
-	Channel,
-	Song,
-	Synth,
-	fastFourierTransform,
-	forwardRealFourierTransform,
-	inverseRealFourierTransform,
-	FilterCoefficients,
-	FrequencyResponse,
-	DynamicBiquadFilter,
-	ColorConfig,
-	EditorConfig,
-	SongDocument,
-	SongEditor,
-	ExportPrompt,
-	ChangePreset,
-};
