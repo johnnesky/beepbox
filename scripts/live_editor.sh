fc8fb75f880841c405834671a64047966d6dd095
John Nesky <johnnesky@gmail.com>
2024-07-15 22:08:20 -0700

    Set a specific port for the live editor server, and skip opening a browser if --headless is passed.

--- scripts/live_editor.sh
+++ scripts/live_editor.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 set -e
 
+# Defaults to opening index_debug.html in a browser, but that can be disabled
+# by passing the argument --headless, like this:
+# npm run live-editor -- --headless
+open_browser_path=/index_debug.html
+for arg in "$@"; do
+  case "$arg" in
+    '--headless') open_browser_path=false;;
+  esac
+done
+
 # Uses the "concurrently" npm package to execute multiple tasks simultaneously.
 # This performs some but not all of the tasks from compile_beepbox_editor.sh.
 # tsc is used to transpile TypeScript files to JavaScript files in build/, and
@@ -15,4 +25,4 @@ set -e
 npx concurrently \
 	"npx tsc -p scripts/tsconfig_editor.json --watch --preserveWatchOutput" \
 	"npx rollup build/editor/index.js --file website/beepbox_editor.js --format iife --output.name beepbox --context exports --sourcemap --plugin rollup-plugin-sourcemaps --plugin @rollup/plugin-node-resolve --watch.buildDelay 200 -w" \
-	"npx five-server --wait=200 --watch=website/* --open=/index_debug.html website/"
+	"npx five-server --wait=200 --watch=website/* --port=8081 --open=$open_browser_path website/"
--- scripts/live_editor_fast_typeless.sh
+++ scripts/live_editor_fast_typeless.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 set -e
 
+# Defaults to opening index_debug.html in a browser, but that can be disabled
+# by passing the argument --headless, like this:
+# npm run live-editor-fast-typeless -- --headless
+open_browser_path=/index_debug.html
+for arg in "$@"; do
+  case "$arg" in
+    '--headless') open_browser_path=false;;
+  esac
+done
+
 # This is similar to live_editor.sh, but instead of compiling with tsc and
 # bundling with rollup, this uses esbuild for both. It uses less resources and
 # is faster. However, this doesn't check type safety at all! Also, the generated
@@ -8,7 +18,4 @@ set -e
 # before publishing updates.
 npx concurrently \
 	"npx esbuild --format=iife --keep-names --global-name=beepbox --bundle ./editor/index.js --outfile=website/beepbox_editor.js --sourcemap --watch" \
-	"npx five-server --wait=200 --watch=website/* --open=/index_debug.html website/"
-
-# To run without automatically opening a web browser, set open to false:
-#	"npx five-server --wait=200 --watch=website/* --open=false website/"
+	"npx five-server --wait=200 --watch=website/* --port=8081 --open=$open_browser_path website/"
